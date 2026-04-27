5f041652b78a7225cf53de877abcd6256d5d6dad
qiujun.orange <qiujun.orange@bytedance.com>
2026-04-27 20:44:49 +0800

    feat: 图标
    Change-Id: I67fe1d01028295d891df91fb61855c1b3fbd4533

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'shortcut icon', href: '/favicon.ico' }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
